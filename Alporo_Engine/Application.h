7c43e2fddfc2cd5d6af5444580a978b3bfb479a8
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2022-09-27 16:27:17 +0200

    80%

--- Alporo_Engine/Application.h
+++ Alporo_Engine/Application.h
@@ -8,12 +8,14 @@
 #include "ModuleInput.h"
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
+#include "ModuleAudio.h"
 
 class Application
 {
 public:
 	ModuleWindow* window;
 	ModuleInput* input;
+	ModuleAudio* audio;
 	ModuleRenderer3D* renderer3D;
 	ModuleCamera3D* camera;
 
