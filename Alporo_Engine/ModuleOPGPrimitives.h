0b402838f372546b894d01fd9245e961ae11a23a
Pol Maresch <polmaresch@gmail.com>
2022-10-14 09:31:49 +0200

    Quad fer per posicions

--- Alporo_Engine/ModuleOPGPrimitives.h
+++ Alporo_Engine/ModuleOPGPrimitives.h
@@ -10,6 +10,20 @@ struct Cilindre
     float Radius = 1;
 };
 
+struct QUad
+{
+    float v0[3] = { 0.f, 1.f, 0.f };
+    float v1[3] = { 0.f, 0.f, 0.f };
+    float v2[3] = { 0.f, 0.f, 1.f };
+    float v3[3] = { 0.f, 1.f, 1.f };
+    float v4[3] = { 1.f, 1.f, 0.f };
+    float v5[3] = { 1.f, 1.f, 1.f, };
+    float v6[3] = { 1.f, 0.f, 1.f, };
+    float v7[3] = { 1.f, 0.f, 0.f, };
+
+
+};
+
 class ModuleOPGPrimitives : public Module
 {
 public:
