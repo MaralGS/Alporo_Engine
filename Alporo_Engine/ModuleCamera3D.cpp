1721783c610b5998ae70590ed6543a8fcfd3168b
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2022-10-27 22:24:11 +0200

    trying alt+leftclikc rotate from object

--- Alporo_Engine/ModuleCamera3D.cpp
+++ Alporo_Engine/ModuleCamera3D.cpp
@@ -106,23 +106,76 @@ update_status ModuleCamera3D::Update(float dt)
 		Position = Reference + Z * length(Position);
 	}
 
+	/*if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT && App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
+	{
+		int dx = -App->input->GetMouseXMotion();
+		int dy = -App->input->GetMouseYMotion();
+
+		float Sensitivity = 0.25f;
+
+		Position -= Reference;
+
+		if (dx != 0)
+		{
+			float DeltaX = (float)dx * Sensitivity;
+
+			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+		}
+
+		if (dy != 0)
+		{
+			float DeltaY = (float)dy * Sensitivity;
+
+			Y = rotate(Y, DeltaY, X);
+			Z = rotate(Z, DeltaY, X);
+
+			if (Y.y < 0.0f)
+			{
+				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
+				Y = cross(Z, X);
+			}
+		}
+		Position = Reference + Z * length(Position);
+
+		if (App->OpenGLPrimitives->PM == 1)
+		{
+			newPos.x = App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2.x + 4;
+			newPos.y = App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2.y + 2;
+			newPos.z = App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2.z + 3;
+			Look(newPos, { App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2 }, FromReference);
+		}
+
+		if (App->OpenGLPrimitives->PM == 2)
+		{
+			newPos.x = App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2.x + 4;
+			newPos.y = App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2.y + 2;
+			newPos.z = App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2.z + 3;
+			Look(newPos, { App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2 }, FromReference);
+		}
+
+	}*/
+
 	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_DOWN && App->OpenGLPrimitives->PM != 0) {
 		if (App->OpenGLPrimitives->PM == 1)
 		{
 			newPos.x = App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2.x + 4;
 			newPos.y = App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2.y + 2;
 			newPos.z = App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2.z + 3;
-			Look(newPos, { App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2}, FromReference);
+			Look(newPos, { App->OpenGLPrimitives->Cub[App->imguiwindows->CubPicked]->Pos2 });
 		}
 		
 		if (App->OpenGLPrimitives->PM == 2)
 		{
-			newPos.x = App->OpenGLPrimitives->Piramid[App->imguiwindows->CubPicked]->Pos2.x + 4;
-			newPos.y = App->OpenGLPrimitives->Piramid[App->imguiwindows->CubPicked]->Pos2.y + 2;
-			newPos.z = App->OpenGLPrimitives->Piramid[App->imguiwindows->CubPicked]->Pos2.z + 3;
-			Look(newPos, { App->OpenGLPrimitives->Piramid[App->imguiwindows->CubPicked]->Pos2}, FromReference);
+			newPos.x = App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2.x + 4;
+			newPos.y = App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2.y + 2;
+			newPos.z = App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2.z + 3;
+			Look(newPos, { App->OpenGLPrimitives->Piramid[App->imguiwindows->PiramidPicked]->Pos2 });
 		}	
 	}
+
+	
 
 	// Recalculate matrix -------------
 	CalculateViewMatrix();
