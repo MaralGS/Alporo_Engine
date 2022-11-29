5a2ab144d043c660019a70f31b95d07e04ee9f93
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2022-11-29 14:29:16 +0100

    Scale and rotation maded

--- Alporo_Engine/Transform.cpp
+++ Alporo_Engine/Transform.cpp
@@ -31,21 +31,21 @@ void Transform::Tranformation()
 {
 	Transform_Matrix.translate(position.x, position.y, position.z);
 
-	Transform_Matrix.scale(scale.x, scale.y, scale.z);
+	//Transform_Matrix.scale(scale.x, scale.y, scale.z);
 
 	//rotation
-	Transform_Matrix[0] = cos(rotate.z) * cos(rotate.x);
+	Transform_Matrix[0] = cos(rotate.z) * cos(rotate.x) * (scale.x * scale.x);
 	Transform_Matrix[4] = cos(rotate.z) * sin(rotate.x);
 	Transform_Matrix[8] = -sin(rotate.z);
 
 	Transform_Matrix[1] = (sin(rotate.y) * sin(rotate.z) * cos(rotate.x)) - (cos(rotate.y) * sin(rotate.x));
-	Transform_Matrix[5] = (sin(rotate.y) * sin(rotate.z) * sin(rotate.x)) + (cos(rotate.y) * cos(rotate.x));
+	Transform_Matrix[5] = ((sin(rotate.y) * sin(rotate.z) * sin(rotate.x)) + (cos(rotate.y) * cos(rotate.x))) * (scale.y * scale.y);
 	Transform_Matrix[9] = sin(rotate.y) * cos(rotate.z);
 
 
 	Transform_Matrix[2] = (cos(rotate.y) * sin(rotate.z) * cos(rotate.x)) + (sin(rotate.y) * sin(rotate.x));
 	Transform_Matrix[6] = (cos(rotate.y) * sin(rotate.z) * sin(rotate.x)) - (sin(rotate.y) * cos(rotate.x));
-	Transform_Matrix[10] = cos(rotate.y) * cos(rotate.z);
+	Transform_Matrix[10] = (cos(rotate.y) * cos(rotate.z))*(scale.z * scale.z) ;
 
 
 
