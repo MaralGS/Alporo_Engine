3aa67fecbbebf1c9201b68103ff84c5d84dcb31a
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2022-12-01 19:10:45 +0100

    not worikng

--- Alporo_Engine/ModuleAssets.cpp
+++ Alporo_Engine/ModuleAssets.cpp
@@ -1,6 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleAssets.h"
+#include "Globals.h"
 
 
 ModuleAssets::ModuleAssets(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -45,4 +46,92 @@ bool ModuleAssets::CleanUp()
 	LOG(LogType::ERRORS, "Unloading Intro Assets");
 
 	return true;
+}
+
+int ModuleAssets::Find(const char* file_in_assets) const
+{
+	return 0;
+}
+
+int ModuleAssets::ImportFile(const char* new_file_in_assets, Resource::Type type)
+{
+	Resource* resource = CreateNewResource(new_file_in_assets, type);
+	int ret = 0;
+	char* fileBuffer = nullptr;//Engine->fileSystem->Load(assetsFile); 
+	switch (resource->GetType()) {
+	//case Resource::texture: App->tex->Import(fileBuffer, resource); break;
+	case Resource::Type::scene: App->scene->Import(fileBuffer, resource); break;
+	case Resource::Type::mesh: App->meshes->Import(fileBuffer, resource); break;
+	}
+	//SaveResource(resource);
+	ret = resource->GetUID();
+	RELEASE_ARRAY(fileBuffer);
+	UnloadResource(ret); 
+	return ret;
+	return 0;
+}
+
+int ModuleAssets::GenerateNewUID()
+{
+	return 0;
+}
+
+const Resource* ModuleAssets::RequestResource(int uid) const
+{
+	std::map<int, Resource*>::iterator it = resources.find(uid);
+	if (it != resources.end())
+	{
+		it->second->referenceCount++;
+		return it->second;
+	}
+
+	return nullptr;
+}
+
+Resource* ModuleAssets::RequestResource(int uid)
+{
+	return nullptr;
+}
+
+void ModuleAssets::UnloadResource(int uid)
+{
+	/*Resource* res = nullptr;
+
+	std::map<int, Resource*>::iterator it = resources.find(uid);
+	if (it == resources.end())
+		return;
+
+	res = it->second;
+	res->DecreaseReferenceCount();
+
+	if (res->GetReferenceCount() <= 0)
+		ReleaseResource(res->GetUID());*/
+}
+
+void ModuleAssets::ReleaseResource(int uid)
+{
+}
+
+Resource* ModuleAssets::CreateNewResource(const char* assetsFile, Resource::Type type)
+{
+
+	Resource* ret = nullptr;
+	UID uid = GenerateNewUID();
+	switch (type) {
+	case Resource::texture: ret = (Resource*) new ResourceTexture(uid); break;
+	case Resource::mesh: ret = (Resource*) new ResourceMesh(uid); break;
+	case Resource::scene: ret = (Resource*) new ResourceScene(uid); break;
+	case Resource::bone: ret = (Resource*) new ResourceBone(uid); break;
+	case Resource::animation: ret = (Resource*) new ResourceAnimation(uid); break;
+	}
+	if (ret != nullptr)
+	{
+		resources[uid] = ret;
+		resource->assetsFile = assetsPath;
+		resource->libraryFile = GenLibraryPath(resource);
+	}
+	return ret;
+
+
+	return nullptr;
 }
--- Alporo_Engine/ModuleAssets.h
+++ Alporo_Engine/ModuleAssets.h
@@ -2,6 +2,8 @@
 #include "Module.h"
 #include "Globals.h"
 #include "ModuleAssets.h"
+#include "Resources.h"
+#include <map>
 
 class ModuleAssets : public Module
 {
@@ -13,6 +15,17 @@ public:
 	update_status PreUpdate(float dt);
 	update_status Update(float dt);
 	bool CleanUp();
+
+	int Find(const char* file_in_assets) const;
+	int ImportFile(const char* new_file_in_assets);
+	int GenerateNewUID();
+	const Resource* RequestResource(int uid) const;
+	Resource* RequestResource(int uid);
+	void ReleaseResource(int uid);
+	private:
+	Resource* CreateNewResource(const char* assetsFile, Resource::Type type);
+	std::map<int, Resource*> resources;
+
 
 public:
 	GameObject* Root = nullptr;
